6cf75753a412be8a1a23b3e01b19fef6a311ff11
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-03-15 12:02:46 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -25,3 +25,31 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********************Operations**********************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Prathamesh"
+
+let str3 = str1 + str2
+console.log(str3);        // Hello Prathamesh
+console.log("1" + 2);     //12
+console.log(1 + "2");     //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log((3 + 4) * 5 % 5);
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
