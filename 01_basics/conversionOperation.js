d363771c6a2afe48c81cdf7d10f37f6db6e315e2
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-03-15 09:22:10 +0000

    a

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -4,8 +4,8 @@ let score = true
 console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
 console.log(valueInNumber);
+console.log(typeof valueInNumber);
 
 
 
