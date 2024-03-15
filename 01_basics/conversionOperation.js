b23edfbe1c239d72f9e60cb81b38eb7a4ed9b808
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-03-15 06:41:37 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => converted into 33
+// "33abc" => NaN (Not A Number)
+// true => 1; false => 0 
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false
+// "Prathamesh" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
