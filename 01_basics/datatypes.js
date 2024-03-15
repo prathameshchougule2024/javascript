3b0d8edb7f99c8e6984a4fc4f1a61dfc45f7f8cc
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-03-15 05:25:10 +0000

    datatypes and vairables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as a newer version
+
+console.log(3+1);
+console.log("Prathamesh");
+
+
+let name = "Prathamesh"
+let age = 21
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object
---
+++ 01_basics/vairables.js
@@ -0,0 +1,14 @@
+const accountId = 144554
+let accountEmail = "account@gmail.com"
+var accountPassword = "123456"
+accountCity = "kolhapur"
+let accountState
+
+console.log(accountId);
+
+accountEmail = "admin@gmail.com"
+accountPassword = "525263"
+accountCity = "Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
