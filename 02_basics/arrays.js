aefce131d8017d4cfea5ad99aefd8369521adc26
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-03-19 11:57:11 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,31 @@
+// //array
+
+const Myarr = [0,1,2,3,4,5]
+const MyHeros = ["Shaktiman", "Superman"]
+console.log(Myarr[1]);   // 1
+console.log(MyHeros[0]);   // Shaktiman
+
+// Array Method
+
+Myarr.push(9)
+console.log(Myarr);   // [ 0, 1, 2, 3, 4, 5, 9 ]
+
+Myarr.pop()
+Myarr.unshift(0)
+Myarr.shift()
+console.log(Myarr);    // [ 0, 1, 2, 3, 4, 5 ]
+
+console.log(Myarr.includes(9)); // false
+console.log(Myarr.indexOf(1)); // 1
+
+
+const newArr = Myarr.join()
+
+console.log(Myarr);
+console.log(newArr);
+ 
+Slice , splice
+console.log("A ", Myarr);
+const Myn1 = Myarr.slice(1,3)
+console.log(Myn1);
+console.log("B ", Myarr);
