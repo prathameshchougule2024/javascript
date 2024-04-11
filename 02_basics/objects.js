53ea4ad1751ed2541c3abf2facb68b9a80319fb7
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-04-11 06:32:14 +0000

    objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename: "BDA",
+    price: "999",
+    courseInstructor: "Prathamesh"
+} 
+const {courseInstructor:instructor} = course
+
+console.log(instructor)  // Prathamesh
+
+// {
+//     "name" = "Prathamesh"
+//     "coursename" = "BDA",
+//     "price" = "free"
+// }
+
+[
+    {},
+    {},
+    {}
+    
+]
+
+above part is a type of JSON format API
