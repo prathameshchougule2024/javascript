8de804cfff9ba9902aa2a60fb198298f6d912294
Prathamesh Dilip Chougule <156286811+prathameshchougule2024@users.noreply.github.com>
2024-04-15 08:04:26 +0000

    scope level

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -3,13 +3,50 @@ if(true){
     let a =10
     const b = 20
     var c = 30
-    console.log("Inner: ",a);
+    //console.log("Inner: ",a);  //10
 }
 // for (let i = 0; i< array.length; i++) {
 //     const element = array[i];
     
 // }
 
-console.log(a);
+//console.log(a);      //300
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "PC"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+
+}
+//one()
+if(true){
+    const username = "PC"
+    if (username === "PC") {
+        const website = " youtube"
+       // console.log(username + website );    
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+// +++++++++++++++++++++++++++Interesting+++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
